52ab0fe0f6893451527dc177decc5f1b8796fb17
Alberto <pujatolon@hotmail.com>
2018-06-23 13:00:19 +0200

    Initial commit

---
+++ src/CleanArchitecture.Web/Controllers/ToDoController.cs
@@ -0,0 +1,50 @@
+﻿using CleanArchitecture.Core.Entities;
+using CleanArchitecture.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace CleanArchitecture.Web.Controllers
+{
+    public class ToDoController : Controller
+    {
+        private readonly IRepository<ToDoItem> _todoRepository;
+
+        public ToDoController(IRepository<ToDoItem> todoRepository)
+        {
+            _todoRepository = todoRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var items = _todoRepository.List();
+            return View(items);
+        }
+
+        public IActionResult Populate()
+        {
+            int recordsAdded = PopulateDatabase();
+            return Ok(recordsAdded);
+        }
+
+        public int PopulateDatabase()
+        {
+            if (_todoRepository.List().Any()) return 0;
+            _todoRepository.Add(new ToDoItem()
+            {
+                Title = "Get Sample Working",
+                Description = "Try to get the sample to build."
+            });
+            _todoRepository.Add(new ToDoItem()
+            {
+                Title = "Review Solution",
+                Description = "Review the different projects in the solution and how they relate to one another."
+            });
+            _todoRepository.Add(new ToDoItem()
+            {
+                Title = "Run and Review Tests",
+                Description = "Make sure all the tests run and review what they are doing."
+            });
+            return _todoRepository.List().Count;
+        }
+    }
+}
---
+++ src/CleanArchitecture.Web/Startup.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using CleanArchitecture.Core.Interfaces;
+using CleanArchitecture.Core.SharedKernel;
+using CleanArchitecture.Infrastructure.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using StructureMap;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace CleanArchitecture.Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration config)
+        {
+            Configuration = config;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            // TODO: Add DbContext and IOC
+            string dbName = Guid.NewGuid().ToString();
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseInMemoryDatabase(dbName));
+                //options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddMvc()
+                .AddControllersAsServices();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
+
+            var container = new Container();
+
+            container.Configure(config =>
+            {
+                config.Scan(_ =>
+                {
+                    _.AssemblyContainingType(typeof(Startup)); // Web
+                    _.AssemblyContainingType(typeof(BaseEntity)); // Core
+                    _.Assembly("CleanArchitecture.Infrastructure"); // Infrastructure
+                    _.WithDefaultConventions();
+                    _.ConnectImplementationsToTypesClosing(typeof(IHandle<>));
+                });
+                
+                // TODO: Add Registry Classes to eliminate reference to Infrastructure
+
+                // TODO: Move to Infrastucture Registry
+                config.For(typeof(IRepository<>)).Add(typeof(EfRepository<>));
+
+                //Populate the container using the service collection
+                config.Populate(services);
+            });
+
+            return container.GetInstance<IServiceProvider>();
+        }
+
+        public void ConfigureTesting(IApplicationBuilder app,
+            IHostingEnvironment env,
+            ILoggerFactory loggerFactory)
+        {
+            this.Configure(app, env, loggerFactory);
+            SeedData.PopulateTestData(app.ApplicationServices.GetService<AppDbContext>());
+        }
+
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
